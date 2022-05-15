f7645711f3cb87db128307adfc68fa5f8e2a81de
Lena <olena.ratushna21@gmail.com>
2022-05-15 18:34:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const sliderMain = new Swiper('.slider_main', {
+    freeMode: true,
+    mousewheel: true,
+    parallax: true,
+    centeredSlides: false,
+    breakpoints: {
+        0: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+            
+        },
+        321: {
+            slidesPerView: 2.5,
+            spaceBetween: 30,
+        },
+        992: {
+            centeredSlides: true,
+            slidesPerView: 2.5,
+            spaceBetween: 50,
+        },
+        1199: {
+            centeredSlides: true,
+            slidesPerView: 3.5,
+            spaceBetween: 60,
+        }
+    }
+});
+
+const sliderBg = new Swiper('.slider_bg', {
+    centeredSlides: true,
+    parallax: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+            
+        },
+        321: {
+            slidesPerView: 2.5,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 2.5,
+            spaceBetween: 50,
+        },
+        1199: {
+            slidesPerView: 3.5,
+            spaceBetween: 60,
+        }
+    }
+});
+
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll('.slider__item').forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('opened');
+    });
+});
+
+if(window.innerWidth > 991) {
+    const desc = document.querySelector('.description');
+    sliderMain.on('snapIndexChange', () => {
+        sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden');
+    });
+}
